00f8dadc1ff683e252b696c73c35f4a454bcc7ea
dariasteam <dariasteam94@gmail.com>
2017-02-13 16:43:22 +0000

    Deleting _book folder

--- _book/gulpfile.js
+++ _book/gulpfile.js
@@ -3,7 +3,9 @@ var ghpages = require('gh-pages');
 var path = require('path');
 
 gulp.task('gh-pages', function(){
-	ghpages.publish(path.join(__dirname, '_book'), function(err) {});
+	ghpages.publish(path.join(__dirname, '_book'), {
+    message: 'Auto update gh-pages branch'
+  }, function(err) {});
 });
 
 gulp.task('default', [ 'gh-pages' ]);
