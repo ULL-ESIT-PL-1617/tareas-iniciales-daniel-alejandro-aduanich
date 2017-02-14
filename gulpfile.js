44c12fd164fc7255b16e08fc761331a78b23a772
dariasteam <dariasteam94@gmail.com>
2017-02-14 09:46:02 +0000

    gulp creates gibook

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 var gulp = require('gulp');
 var ghpages = require('gh-pages');
 var path = require('path');
+var shell = require('gulp-shell')
 
-gulp.task('gh-pages', function(){
+
+gulp.task('gitbook', shell.task('gitbook build'))
+
+gulp.task('gh-pages', ['gitbook'], function(){
 	ghpages.publish(path.join(__dirname, '_book'), {
     message: 'Auto update gh-pages branch'
   }, function(err) {});
 });
 
-gulp.task('default', [ 'gh-pages' ]);
+gulp.task('default', ['gitbook', 'gh-pages' ]);
